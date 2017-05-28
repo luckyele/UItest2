50b363b37836abe8409c6b88643c0f7be36d1f4e
Yung <541913141@qq.com>
2017-05-28 21:27:07 +0800

    First commit

---
+++ app/src/main/java/com/example/lucky/uitest2/FirstActivity.java
@@ -0,0 +1,17 @@
+package com.example.lucky.uitest2;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+
+
+public class FirstActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+    }
+
+}
---
+++ app/src/main/java/com/example/lucky/uitest2/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.lucky.uitest2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, FirstActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, 3000);
+    }
+}
